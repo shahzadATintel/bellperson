35ecb6b408d485e534229eda0d9457b4adbdfbc9
Shahzad Ahmad Butt <shahzad.ahmad.butt@intel.com>
2023-09-29 18:13:21 -0700

    updated test to be launched and override for rust version

--- env_setup.sh
+++ env_setup.sh
@@ -4,3 +4,4 @@ export BELLMAN_CPU_UTILIZATION=0
 export BELLMAN_VERIFIER=gpu
 export RUST_GPU_TOOLS_CUSTOM_GPU="Tesla V100:5120"
 export BELLMAN_GPU_FRAMEWORK=cuda
+rustup override set 1.66.0
--- run_msm.sh
+++ run_msm.sh
@@ -1,1 +1,3 @@
-RUSTFLAGS="-C target-cpu=native" cargo test --release --all --features cuda,opencl
+source ./env_setup.sh
+#RUSTFLAGS="-C target-cpu=native" cargo test --release --all --features cuda,opencl
+RUST_LOG=info cargo test --features cuda,opencl -- --exact groth16::multiscalar::tests::test_multiscalar_par --nocapture
